432b0a2b91b3d54ac0ae8ee11e86f6047d393428
fatmakazimova26 <fatmakazimova26@gmail.com>
2023-06-22 19:55:42 +0400

    created title component

--- ui/public/src/pages/Login.jsx
+++ ui/public/src/pages/Login.jsx
@@ -8,22 +8,7 @@ import { LongBtn } from "../components/buttons/LongBtn";
 function Login() {
   return (
     <div>
-      <LongBtn className="long-gray" text="hello" />
-      <LongBtn className="long-green-1" text="hello" />
-      <LongBtn className="long-green-2" text="hello" />
-      <LongBtn className="outlined-green" text="hello" />
-      <IconBtn icon="facebook" />
-      <IconBtn icon="google" />
-      <IconBtn icon="apple" />
-      {/* <Button text="btn" className="default-gray" />
-      <Button text="btn" className="default-green-1" />
-      <Button text="btn" className="default-green-2" />
-      <Button text="btn" className="outlined-green" /> */}
-      {/* <LongBtn text={"Hello world"} bg="green" /> */}
-      {/* <IconBtn icon="google" />
-      <IconBtn icon="facebook" />
-      <IconBtn icon="apple" />
-      <LinkBtn text="link btn" /> */}
+     Login
     </div>
   );
 }
