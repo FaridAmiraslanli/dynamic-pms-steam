37fbd3904877875738314eeb49f0ed2e0128d841
ccavad <ccavad2015@gmail.com>
2023-06-22 16:35:12 +0400

    buttons sass

---
+++ ui/public/src/components/buttons/LongBtn.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./buttons.scss"
+
+export function LongBtn({className, text}) {
+  return (
+    <button className={className}>{text}</button>
+  )
+}
--- ui/public/src/pages/Login.jsx
+++ ui/public/src/pages/Login.jsx
@@ -1,13 +1,29 @@
-import { Button, IconBtn, LinkBtn } from "../components/Button";
+// import { Button, IconBtn, LinkBtn, LongBtn } from "../components/MuiButtons";
+// import {LongBtn} from "../components/buttons/LongBtn";
+
+import { IconBtn } from "../components/buttons/IconBtn";
+import { LongBtn } from "../components/buttons/LongBtn";
+
 
 function Login() {
   return (
     <div>
-      <Button text="btn" />
+      <LongBtn className="long-gray" text="hello" />
+      <LongBtn className="long-green-1" text="hello" />
+      <LongBtn className="long-green-2" text="hello" />
+      <LongBtn className="outlined-green" text="hello" />
+      <IconBtn icon="facebook" />
       <IconBtn icon="google" />
+      <IconBtn icon="apple" />
+      {/* <Button text="btn" className="default-gray" />
+      <Button text="btn" className="default-green-1" />
+      <Button text="btn" className="default-green-2" />
+      <Button text="btn" className="outlined-green" /> */}
+      {/* <LongBtn text={"Hello world"} bg="green" /> */}
+      {/* <IconBtn icon="google" />
       <IconBtn icon="facebook" />
       <IconBtn icon="apple" />
-      <LinkBtn text="link btn" />
+      <LinkBtn text="link btn" /> */}
     </div>
   );
 }
