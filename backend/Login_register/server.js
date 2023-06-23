f3b68238cd5d94970c4e922a13e0c64007ed6d23
ORxanone <recebovorxan3@gmail.com>
2023-06-23 12:47:40 +0400

    registration_swaggerAPI

--- backend/Login_register/server.js
+++ backend/Login_register/server.js
@@ -14,27 +14,11 @@ app.set("view enjine", "ejs");
 const PORT = process.env.PORT || 8080;
 
 
-// swagger
-const swaggerOption = {
-  swaggerDefinition: {
-    info: {
-      title: "http://localhost:8080/registration",
-      version: "1.0.0",
-      description: "http://localhost:8080/registration",
-    },
-  },
-  apis: ["./user.js"],
-};
+app.use("/", require("./routers/user.js"));
+
 const swaggerDocs = require("./swagger.json");
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
-app.use("/", require("./routers/user.js"));
-// swagger
-
-app.set("view engine", "ejs");
-
-app.use("/", userRouter);
 
 connect();
 app.listen(PORT, () => {
