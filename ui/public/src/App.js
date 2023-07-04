e8a2e89973eae3d03ed1cfe83e85185b4c1faab9
SabinaGarayeva <sgarayeva13@gmail.com>
2023-07-04 19:03:33 +0400

    Credit_packs_1

--- ui/public/src/App.js
+++ ui/public/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
+
 import Login from "@pages/Login";
 import Home from "@pages/Home";
 import Register from "@pages/Register";
@@ -15,6 +16,7 @@ import { userStore } from "./store/userStore";
 // import {AnimatePresence} from "framer-motion/dist/framer-motion";
 import {AnimatePresence} from "framer-motion";
 import './assets/sass/globals.scss'
+import Pricing from "./components/pricingPage/Pricing";
 const App = () => {
   // TODO .env fayli olmalidi backend islemesi ucun
   // TODO snackbar mui
@@ -31,25 +33,29 @@ const App = () => {
   }, []);
 
   return (
-    <AnimatePresence>
-      {authKey ? (
-        <Routes location={location} key={location.pathname}>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Error />} />
-          {/* <Route path="/login" /> */}
-        </Routes>
-      ) : (
-        <Routes location={location} key={location.pathname}>
-          <Route exact path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/creditpacks" element={<CreditPacks />} />
-          <Route path="/forget" element={<Forget />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      )}
-    </AnimatePresence>
+
+  <div>
+    <Pricing/>
+  </div>
+    // <AnimatePresence>
+    //   {authKey ? (
+    //     <Routes location={location} key={location.pathname}>
+    //       <Route exact path="/" element={<Home />} />
+    //       <Route path="/home" element={<Home />} />
+    //       <Route path="*" element={<Error />} />
+    //       {/* <Route path="/login" /> */}
+    //     </Routes>
+    //   ) : (
+    //     <Routes location={location} key={location.pathname}>
+    //       <Route exact path="/" element={<Login />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/register" element={<Register />} />
+    //       <Route path="/creditpacks" element={<CreditPacks />} />
+    //       <Route path="/forget" element={<Forget />} />
+    //       <Route path="*" element={<Error />} />
+    //     </Routes>
+    //   )}
+    // </AnimatePresence>
   );
 };
 
